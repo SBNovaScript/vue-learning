6ba794fced18402e74c0137a16e7883a5dd4f334
Steven Baumann <steven.baumann@mymail.champlain.edu>
2020-10-26 23:51:53 -0400

    updated to vue 3.

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-Vue.component('message', {
-    props: ['data'],
-    template: '<p>{{data}}</p>'
-})
-
-let app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello!'
-    },
-    methods: {
-        reverseMessage: function() {
-            this.message = this.message.split('').reverse().join('')
+const Counter = {
+    data() {
+        return {
+            counter: 0,
+            shouldCount: false
         }
     },
-});
+    mounted() {
+        setInterval(() => {
+            this.shouldCount ? this.counter++ : null
+        }, 250)
+    },
+    methods: {
+        toggleCount() {
+            this.shouldCount = !this.shouldCount;
+        }
+    },
+};
+
+Vue.createApp(Counter).mount('main');
